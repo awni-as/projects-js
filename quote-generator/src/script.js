a59659a1809e52c0660173b90e56e7324f8e9988
Awni <awniabsh@gmail.com>
2021-10-09 08:05:24 +0300

    Replaced Quote & Author with Dynamic Content

--- quote-generator/src/script.js
+++ quote-generator/src/script.js
@@ -1,4 +1,8 @@
-const quoteContainer = document.getElementById("quote-generator");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
 let apiQuotes = [];
 
@@ -6,6 +10,8 @@ let apiQuotes = [];
 function newQuote() {
   // Pick a random quote from apiQuotes array.
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
