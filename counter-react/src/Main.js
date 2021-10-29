9934586f8b9b77c55375e737f2ea5b297c821f74
Awni <awniabsh@gmail.com>
2021-10-29 08:45:36 +0200

    Added Logic

--- counter-react/src/Main.js
+++ counter-react/src/Main.js
@@ -2,17 +2,38 @@ import React, { Component } from "react";
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      number: 0,
+    };
   }
+
+  increment = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+  decrement = () => {
+    this.setState({ number: this.state.number - 1 });
+  };
+  reset = () => {
+    this.setState({ number: 0 });
+  };
   render() {
     return (
       <div class="row justify-content-center">
         <div className="col-md-6">
           <h1> Counter </h1>
-          <h1>0 </h1>
-          <button className="btn btn-success"> Increment </button>
-          <button className="btn btn-danger"> Decrement </button>
-          <button className="btn btn-primary"> Reset </button>
+          <h1>{this.state.number} </h1>
+          <button className="btn btn-success" onClick={this.increment}>
+            {" "}
+            Increment{" "}
+          </button>
+          <button className="btn btn-danger" onClick={this.decrement}>
+            {" "}
+            Decrement{" "}
+          </button>
+          <button className="btn btn-primary" onClick={this.reset}>
+            {" "}
+            Reset{" "}
+          </button>
         </div>
       </div>
     );
