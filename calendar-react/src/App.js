b1eee25576f573f98ee10e2876ed4279ce841a18
Awni <awniabsh@gmail.com>
2021-12-27 13:25:36 +0200

    Added Skeleton Hooks

--- calendar-react/src/App.js
+++ calendar-react/src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Day from "./components/Day";
 
 function App() {
+  // 0 refers to current month. -1 refers to the previous month. 1 refers to the next month.
+  const [nav, setNav] = useState(0);
+  const [days, setDays] = useState([]);
+  const [dateDisplay, setDateDisplay] = useState("");
+  // Initialized as null.
+  const [clicked, setClicked] = useState();
+  // Checks if there is an event item in the local storage. If not, events are set to an empty array.
+  const [events, setEvents] = useState(
+    localStorage.getItem("events")
+      ? JSON.parse(localStorage.getItem("events"))
+      : []
+  );
+
+  function eventForDate(date) {
+    events.find((e) => e.date === date);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("events", JSON.stringify(events));
+  }, [events]);
+
   return (
     <div>
       <Day />
