663cb24794ff6a3d3f6c6ea693ce9939132dfae8
Awni <awniabsh@gmail.com>
2022-01-15 12:58:28 +0200

    Styled Menu Items

--- e-commerce-react/src/components/menu-item/menu-item.component.jsx
+++ e-commerce-react/src/components/menu-item/menu-item.component.jsx
@@ -3,12 +3,13 @@ import "./menu-item.styles.scss";
 
 function MenuItem(props) {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${props.imageURL})`,
-      }}
-      className={`menu-item ${props.size}`}
-    >
+    <div className={`menu-item ${props.size}`}>
+      <div
+        style={{
+          backgroundImage: `url(${props.imageURL})`,
+        }}
+        className="background-image"
+      ></div>
       <div className="content">
         <h1 className="title">{props.title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
