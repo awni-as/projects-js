557209ee98c8818ef2d6b5936929a77bb071f37d
Awni <awniabsh@gmail.com>
2022-01-15 10:10:03 +0200

    Added HomePage Component

--- e-commerce-react/src/App.js
+++ e-commerce-react/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./components/homepage.component";
 
 function App() {
   return (
     <div>
+      <HomePage />
       <h1> Hello World! </h1>
     </div>
   );
---
+++ e-commerce-react/src/components/homepage.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomePage() {
+  return <div>Hello from the HomePage!</div>;
+}
+
+export default HomePage;
