4224e53f2083f8183b2845828c2278d20a05fe5b
Awni <awniabsh@gmail.com>
2022-01-18 06:35:43 +0200

    Added Shop Page & Components

--- e-commerce-react/src/App.js
+++ e-commerce-react/src/App.js
@@ -3,21 +3,15 @@ import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/shop/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
-      {/* <HomePage /> */}
     </div>
   );
 }
---
+++ e-commerce-react/src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./collection-item.styles.scss";
+
+function CollectionItem(props) {
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{ backgroundImage: `url(${props.imageUrl})` }}
+      ></div>
+      <div className="collection-footer">
+        <span className="name">{props.name}</span>
+        <span className="price">{props.price}</span>
+      </div>
+    </div>
+  );
+}
+
+export default CollectionItem;
