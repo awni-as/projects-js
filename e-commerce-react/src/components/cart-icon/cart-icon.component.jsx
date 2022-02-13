23ee2bfe1c5f62211f2cf0f8a8f138c7a1b23cf1
Awni <awniabsh@gmail.com>
2022-02-13 03:56:36 +0200

    Acculmated Quantity to display itemCount

--- e-commerce-react/src/components/cart-icon/cart-icon.component.jsx
+++ e-commerce-react/src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
@@ -9,12 +9,18 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
 function CartIcon() {
+  const cartItems = useSelector((state) => state.cart.cartItems);
   const dispatch = useDispatch();
+
+  const itemCount = cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  );
 
   return (
     <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 }
