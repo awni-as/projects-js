40b496a351ad46a3ba6e6dbdc195b58b80612007
Awni <awniabsh@gmail.com>
2022-03-19 18:09:11 +0300

    Fixed Typo

--- e-commerce-react/src/views/SignInAndSignUp/SignUp/SignUp.component.jsx
+++ e-commerce-react/src/views/SignInAndSignUp/SignUp/SignUp.component.jsx
@@ -44,7 +44,7 @@ function SignUp() {
 
   return (
     <div className="sign-up">
-      <h2 className="title">I do not have a account</h2>
+      <h2 className="title">I do not have an account</h2>
       <span>Sign up with your email and password</span>
 
       <form className="sign-up-form" onSubmit={signUpFormSubmissionHandler}>
