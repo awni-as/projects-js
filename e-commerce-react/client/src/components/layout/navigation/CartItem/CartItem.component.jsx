4bd60b9ad132675c44c13ee1672deb906f09eb27
Awni <awniabsh@gmail.com>
2022-03-25 12:57:54 +0300

    CartItem Styles into Styled Components

--- e-commerce-react/client/src/components/layout/navigation/CartItem/CartItem.component.jsx
+++ e-commerce-react/client/src/components/layout/navigation/CartItem/CartItem.component.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 
-import "./CartItem.styles.scss";
+import {
+  CartItemContainer,
+  ItemDetailsContainer,
+  CartItemImage,
+} from "./CartItem.styles";
 
 const CartItem = (props) => {
   return (
-    <div className="cart-item">
-      <img src={props.imageUrl} alt="item"></img>
-      <div className="item-details">
+    <CartItemContainer>
+      <CartItemImage src={props.imageUrl} alt="item"></CartItemImage>
+      <ItemDetailsContainer>
         <span className="name">{props.name}</span>
         <span className="price">
           {props.quantity} x ${props.price}
         </span>
-      </div>
-    </div>
+      </ItemDetailsContainer>
+    </CartItemContainer>
   );
 };
 
