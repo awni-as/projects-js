b75d6d54302a82abadce135535ff168b4e9c3684
Awni <awniabsh@gmail.com>
2022-07-13 06:05:16 +0300

    Added File & Folder Structure + Routes

--- fitness-app-react/src/App.js
+++ fitness-app-react/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import Navigation from "./components/layout/navigation/Navigation/Navigation.component";
+import HomePage from "./views/Home/HomePage/HomePage.component";
+import ExerciseDetailPage from "./views/ExerciseDetail/ExerciseDetailPage/ExerciseDetailPage.component";
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <Box width="400px">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/exercise/:id" element={<ExerciseDetailPage />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
