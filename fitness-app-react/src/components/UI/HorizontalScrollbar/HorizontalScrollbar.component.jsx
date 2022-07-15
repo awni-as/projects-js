b89d8509412af421f75a46e52ee99d9c61641d2d
Awni <awniabsh@gmail.com>
2022-07-15 19:07:12 +0300

    Test

--- fitness-app-react/src/components/UI/HorizontalScrollbar/HorizontalScrollbar.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function HorizontalScrollbar() {
-  return <div>HorizontalScrollbar</div>;
-}
-
-export default HorizontalScrollbar;
--- fitness-app-react/src/views/ExerciseDetail/ExerciseDetailPage/ExerciseDetailPage.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ExerciseDetailPage = () => {
-  return <div>ExerciseDetailPage</div>;
-};
-
-export default ExerciseDetailPage;
